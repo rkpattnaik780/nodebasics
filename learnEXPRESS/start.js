7b8e99122789ad720c16fa9d05dfc4296673c11f
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2017-06-20 14:09:16 +0530

    Stylus module added.

--- learnEXPRESS/start.js
+++ learnEXPRESS/start.js
@@ -1,7 +1,8 @@
 var server1 = process.argv[2] ;
 var express = require('express') ;
 var app = express() ;
-app.get('/home' , function(req,res){
+app.get('/home' , function(req,res){  // app.get()  is part of Express' application routing and is intended 
+                                      //for matching and handling a specific route when requested with the GET HTTP verb
     res.end("Hello World!") ;
 });
 app.listen(server1) ;
--- learnEXPRESS/task3.js
+++ learnEXPRESS/task3.js
@@ -5,11 +5,13 @@ var path = require('path') ;
 var express = require('express') ;
 var app = express( ) ;
 
-app.set('views' , path.join(__dirname , 'templates')) ;
-app.set( 'view engine' , 'pug') ;
+app.set('views' , path.join(__dirname , 'templates')) ; // tells the directory where template files are located , directs to the index page
+app.set( 'view engine' , 'pug') ; // sets template to pug.
 app.use(express.static(process.argv[3])) ;
-app.get('/home' , function(req,res){
+//console.log(process.argv[3]) ;
+app.get('/home' , function(req,res){   // /home is the route at which the request will run.
     res.render('index', { date : new Date().toDateString()}) ;
-}) ;
+}) ;  // res.render() function compiles your template , 
+      // inserts locals there, and creates html output out of those two things.
 
 app.listen(server1) ;
