7535b31b2f5ab3b27df83e6fd3919a2f13d546e5
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2017-06-24 01:53:36 +0530

    Pam Pam Param

--- learnEXPRESS/task6.js
+++ learnEXPRESS/task6.js
@@ -4,3 +4,17 @@ In cryptography, SHA-1 (Secure Hash Algorithm 1) is a cryptographic hash functio
   SHA-1 produces a 160-bit (20-byte)
  hash value known as a message digest. A SHA-1 hash value is typically rendered as a hexadecimal number, 40 digits long.
 */
+
+var express = require('express') ;
+var app = express() ;
+
+app.put('/message/:id', function(req, res) {
+  var id = req.params.id ;
+  var str = require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex') ;
+  res.send(str) ;
+})
+
+app.listen(process.argv[2]) ;
