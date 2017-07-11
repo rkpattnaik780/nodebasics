00402abb8b32222ac89c139e717b4e386f350588
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2017-07-12 00:23:12 +0530

    IP address fixed

--- headerparser_fcc/routes/index.js
+++ headerparser_fcc/routes/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var useragent = require('express-useragent');
+var app = express() ;
+app.use(useragent.express());
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ "platform" : req.headers['user-agent'] ,
-             "language" : req.headers['accept-language'],
-             "ipaddress" : req.connection.remoteAddress}) ;
+
+  res.json({
+             "platform" : req.headers['user-agent'] ,
+             "language" : req.headers['accept-language'].split(",")[0],
+             "ipaddress" : req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress}) ;  
 });
 
 module.exports = router;
